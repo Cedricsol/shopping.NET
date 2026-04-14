cb2a6d269bddb01b4adbacfb6f12d61aacf52ddc
Cédric <cedric.gautier@consultants-solutec.fr>
2026-04-14 16:55:46 +0200

    add backend

---
+++ Shopping.NET/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shopping.NET.Models;
+
+namespace Shopping.NET.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class ProductController : ControllerBase
+    {
+        private readonly AppDbContext _dbContext;
+        public ProductController (AppDbContext context)
+        {
+            _dbContext = context;
+        }
+
+        [HttpGet]
+        [Route("register")]
+        public IActionResult RegisterProduct(Product product)
+        {
+            var canConnect = _dbContext.Database.CanConnect();
+            if (!canConnect) {
+                return StatusCode(500, "DB connection failed.");
+            }
+            _dbContext.Add(product);
+            _dbContext.SaveChanges();
+            return Ok(product);
+        }
+    }
+}
---
+++ Shopping.NET/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Shopping.NET.Models
+
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; } //use decimal cause more precise
+    }
+}
