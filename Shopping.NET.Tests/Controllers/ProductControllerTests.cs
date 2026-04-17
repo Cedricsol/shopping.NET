9bf1b6afd1514e59d11b7d47484bf585a8ed6966
Cédric <cedric.gautier@consultants-solutec.fr>
2026-04-17 15:01:45 +0200

    update backend tests

---
+++ Shopping.NET.Tests/Controllers/ProductControllerTests.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Shopping.NET.Controllers;
+using Shopping.NET.Models;
+using Shopping.NET.Services;
+
+namespace Shopping.NET.Tests.Controllers
+{
+    public class ProductControllerTests
+    {
+        [Fact]
+        public void RegisterProduct_Should_Add_Product_And_Return_Ok()
+        {
+            // Setup
+            var mockService = new Mock<IProductService>();
+
+            var product = new Product
+            {
+                Name = "Test",
+                Price = 10,
+                ImageUrl = "img.jpg",
+            };
+
+            mockService
+                .Setup(s => s.CreateProduct(product))
+                .Returns(product);
+
+            var controller = new ProductController(mockService.Object);
+
+
+            var result = controller.RegisterProduct(product);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnedProduct = Assert.IsType<Product>(okResult.Value);
+
+            Assert.Equal("Test", returnedProduct.Name);
+        }
+
+        [Fact]
+        public void GetProducts_Should_Return_All_Products()
+        {
+            // Setup
+            var mockService = new Mock<IProductService>();
+
+            var fakeProducts = new List<Product>
+            {
+                new Product {Name = "P1", Price = 10, ImageUrl = "p1.jpg"},
+                new Product {Name = "P2", Price = 42, ImageUrl = "p2.jpg"}
+            };
+
+            mockService
+                .Setup(s => s.GetAllProducts())
+                .Returns(fakeProducts);
+
+            var controller = new ProductController(mockService.Object);
+
+            var result = controller.GetProducts();
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var products = Assert.IsType<List<Product>>(okResult.Value);
+
+            Assert.Equal(2, products.Count);
+        }
+    }
+}
---
+++ Shopping.NET.Tests/Services/ProductServiceTests.cs
@@ -0,0 +1,49 @@
+﻿
+
+using Shopping.NET.Models;
+using Shopping.NET.Services;
+
+namespace Shopping.NET.Tests.Services
+{
+    public class ProductServiceTests
+    {
+        [Fact]
+        public void CreateProduct_Should_Add_Product_To_Db()
+        {
+            // Setup
+            var context = TestDbContextFactory.Create();
+            var service = new ProductService(context);
+
+            var product = new Product
+            {
+                Name = "Test",
+                Price = 10,
+                ImageUrl = "test.jpg",
+            };
+
+            var result = service.CreateProduct(product);
+
+            // Assert
+            Assert.Equal(1, context.Products.Count());
+            Assert.Equal("Test", result.Name);
+        }
+
+        [Fact]
+        public void GetAllProducts_Should_Return_All_Products()
+        {
+            // Setup
+            var context = TestDbContextFactory.Create();
+
+            context.Products.Add(new Product { Name = "P1", Price = 10, ImageUrl = "p1.jpg"});
+            context.Products.Add(new Product { Name = "P2", Price = 42, ImageUrl = "p2.jpg" });
+            context.SaveChanges();
+
+            var service = new ProductService(context);
+
+            var result = service.GetAllProducts();
+
+            Assert.Equal(2, result.Count);
+            Assert.Equal("P1", result[0].Name);
+        }
+    }
+}
