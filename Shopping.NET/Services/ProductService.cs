fe6ae3c4205b0c62e7e22bf390c1d568c35d8e90
Cédric <cedric.gautier@consultants-solutec.fr>
2026-04-17 15:57:03 +0200

    add logger in service

--- Shopping.NET/Services/ProductService.cs
+++ Shopping.NET/Services/ProductService.cs
@@ -5,21 +5,27 @@ namespace Shopping.NET.Services
     public class ProductService : IProductService
     {
         private readonly AppDbContext _dbContext;
+        private readonly ILogger<ProductService> _logger;
 
-        public ProductService(AppDbContext context)
+        public ProductService(AppDbContext context, ILogger<ProductService> logger)
         {
             _dbContext = context;
+            _logger = logger;
         }
 
         public Product CreateProduct(Product product)
         {
             if (!_dbContext.Database.CanConnect())
             {
+                _logger.LogError("Database connection failed");
                 throw new Exception("DB connection failed");
             }
 
             _dbContext.Products.Add(product);
             _dbContext.SaveChanges();
+
+            _logger.LogInformation("Product created with ID: {Id}", product.Id);
+
             return product;
         }
 
@@ -27,8 +33,11 @@ namespace Shopping.NET.Services
         {
             if (!_dbContext.Database.CanConnect())
             {
+                _logger.LogError("Database connection failed");
                 throw new Exception("DB connection failed");
             }
+
+            _logger.LogInformation("Size of the list of products : " + _dbContext.Products.ToList().Count().ToString());
 
             return _dbContext.Products.ToList();
         }
