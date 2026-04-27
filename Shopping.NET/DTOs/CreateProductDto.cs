78cdb3fb3fc71224527b928f33156d5f718195e7
Cédric <cedric.gautier@consultants-solutec.fr>
2026-04-27 16:50:46 +0200

    fix range in backend validation and translate error messages in french

--- Shopping.NET/DTOs/CreateProductDto.cs
+++ Shopping.NET/DTOs/CreateProductDto.cs
@@ -5,11 +5,11 @@ namespace Shopping.NET.DTOs
     public class CreateProductDto
     {
         // All data gotten from front
-        [Required(ErrorMessage = "Name is mandatory")]
+        [Required(ErrorMessage = "Veuillez entrer un nom de produit")]
         public string Name { get; set; } = String.Empty;
-        [Range(0.01, double.MaxValue, ErrorMessage = "Price must be higher than 0 or equal to 0")]
+        [Range(0.00, double.MaxValue, ErrorMessage = "Le prix ne peut pas être négatif")]
         public decimal Price { get; set; }
-        [Required(ErrorMessage = "Image is mandatory")]
+        [Required(ErrorMessage = "Veuillez entrer un chemin d'accès pour l'image sous le format '/images/file-name.extension'")]
         public string ImageUrl { get; set; } = String.Empty;
     }
 }
