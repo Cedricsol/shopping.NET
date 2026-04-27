13983a5ad5c228b229162ba61df1e8c1e1c3acb4
Cédric <cedric.gautier@consultants-solutec.fr>
2026-04-27 16:39:13 +0200

    add validation in backend

--- Shopping.NET/DTOs/CreateProductDto.cs
+++ Shopping.NET/DTOs/CreateProductDto.cs
@@ -1,10 +1,15 @@
-﻿namespace Shopping.NET.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shopping.NET.DTOs
 {
     public class CreateProductDto
     {
         // All data gotten from front
+        [Required(ErrorMessage = "Name is mandatory")]
         public string Name { get; set; } = String.Empty;
+        [Range(0.01, double.MaxValue, ErrorMessage = "Price must be higher than 0 or equal to 0")]
         public decimal Price { get; set; }
+        [Required(ErrorMessage = "Image is mandatory")]
         public string ImageUrl { get; set; } = String.Empty;
     }
 }
