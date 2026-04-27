0ae22a1fe5bd555677177bd7c7ece6c0aa8b297a
Cédric <cedric.gautier@consultants-solutec.fr>
2026-04-27 15:45:49 +0200

    fix comment

--- Shopping.NET/DTOs/CreateProductDto.cs
+++ Shopping.NET/DTOs/CreateProductDto.cs
@@ -2,7 +2,7 @@
 {
     public class CreateProductDto
     {
-        // All data sent to front
+        // All data gotten from front
         public string Name { get; set; } = String.Empty;
         public decimal Price { get; set; }
         public string ImageUrl { get; set; } = String.Empty;
