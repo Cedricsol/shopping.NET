5f38f9a7691c42a18f545a7bfb02d8defa594af8
Cédric <cedric.gautier@consultants-solutec.fr>
2026-04-28 10:35:03 +0200

    update ProductForm tests to test frontend validation

--- frontend/src/components/__tests__/ProductForm.spec.ts
+++ frontend/src/components/__tests__/ProductForm.spec.ts
@@ -3,6 +3,7 @@ import { beforeEach, describe, expect, it, vi } from 'vitest'
 import ProductForm from '@/components/ProductForm.vue'
 import * as productService from '@/services/productService'
 import type { InternalAxiosRequestConfig } from 'axios'
+import { nextTick } from 'vue'
 
 vi.mock('@/services/productService', () => ({
   postProduct: vi.fn(),
@@ -43,12 +44,15 @@ describe('ProductForm.vue', () => {
 
     // Submit form
     await wrapper.find('form').trigger('submit.prevent')
+    // wait DOM to update
+    await nextTick()
 
     expect(productService.postProduct).toHaveBeenCalledWith({
       name: 'Produit test',
       price: 10,
       imageUrl: 'http://image.com',
     })
+    expect(wrapper.text()).not.toContain('Erreur')
 
     // Test success message
     expect(wrapper.text()).toContain('Produit ajouté au shop !')
@@ -65,8 +69,53 @@ describe('ProductForm.vue', () => {
 
     vi.spyOn(productService, 'postProduct').mockRejectedValueOnce(new Error('API error'))
 
+    await wrapper.find('input[placeholder="Nom du produit"]').setValue('Produit test')
+    await wrapper.find('input[placeholder="Prix du produit"]').setValue('10')
+    await wrapper.find('input[placeholder="URL de l\'image"]').setValue('/images/test.jpg')
+
     await wrapper.find('form').trigger('submit.prevent')
+
+    expect(productService.postProduct).toHaveBeenCalled()
 
     expect(wrapper.text()).toContain("Erreur lors de l'ajout du produit")
   })
+
+  //#region Test frontend validation
+  it('Display validation error if name is empty', async () => {
+    const wrapper = mount(ProductForm)
+
+    await wrapper.find('input[placeholder="Prix du produit"]').setValue('10')
+    await wrapper.find('input[placeholder="URL de l\'image"]').setValue('/images/test.jpg')
+
+    await wrapper.find('form').trigger('submit.prevent')
+
+    expect(wrapper.text()).toContain('Veuillez entrer un nom')
+    expect(productService.postProduct).not.toHaveBeenCalled()
+  })
+
+  it('Display validation error if price is negative', async () => {
+    const wrapper = mount(ProductForm)
+
+    await wrapper.find('input[placeholder="Nom du produit"]').setValue('Produit test')
+    await wrapper.find('input[placeholder="Prix du produit"]').setValue('-10')
+    await wrapper.find('input[placeholder="URL de l\'image"]').setValue('/images/test.jpg')
+
+    await wrapper.find('form').trigger('submit.prevent')
+
+    expect(wrapper.text()).toContain('Le prix ne doit pas être négatif')
+    expect(productService.postProduct).not.toHaveBeenCalled()
+  })
+
+  it('Display validation error if image URL is empty', async () => {
+    const wrapper = mount(ProductForm)
+
+    await wrapper.find('input[placeholder="Nom du produit"]').setValue('Produit test')
+    await wrapper.find('input[placeholder="Prix du produit"]').setValue('10')
+
+    await wrapper.find('form').trigger('submit.prevent')
+
+    expect(wrapper.text()).toContain("Veuillez entrer un chemin d'accès pour l'image")
+    expect(productService.postProduct).not.toHaveBeenCalled()
+  })
+  //#endregion
 })
