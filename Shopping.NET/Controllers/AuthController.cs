8b29fc055196fb59f96c5eace4c5e625552747ff
Cédric <cedric.gautier@consultants-solutec.fr>
2026-04-29 17:17:31 +0200

    add authentication controller

---
+++ Shopping.NET/Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shopping.NET.DTOs;
+using Shopping.NET.Services;
+
+namespace Shopping.NET.Controllers
+{
+    [ApiController]
+    [Route("api/auth")]
+    public class AuthController : ControllerBase
+    {
+        private readonly AuthService _authService;
+
+        public AuthController(AuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+        {
+            var result = await _authService.Login(loginDto);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
+        {
+            var user = await _authService.Register(registerDto);
+            return Ok(user);
+        }
+    }
+}
