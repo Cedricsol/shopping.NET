b4b1575e5abc03b9437cf3f9d29040710861bc74
Cédric <cedric.gautier@consultants-solutec.fr>
2026-05-05 15:37:47 +0200

    add backend validation for register

--- Shopping.NET/DTOs/RegisterDto.cs
+++ Shopping.NET/DTOs/RegisterDto.cs
@@ -6,13 +6,17 @@ namespace Shopping.NET.DTOs
     {
         [Required(ErrorMessage = "Veuillez entrer un email")]
         [EmailAddress(ErrorMessage = "Format d'email invalide")]
+        [MaxLength(255, ErrorMessage = "Email trop long")]
         public string Email { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Veuillez entrer un nom d'utilsateur")]
+        [MinLength(3, ErrorMessage = "Le nom d'utilisateur doit contenir au minimum 3 caractères")]
+        [MaxLength(50, ErrorMessage = "Le nom d'utilisateur est trop long")]
+        [RegularExpression(@"^[a-zA-Z0-9_-+$", ErrorMessage = "Cararctères invalides")]
         public string Username { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Veuillez entrer un mot de passe")]
-        [MinLength(6, ErrorMessage = "Le mot de passe doit contenir au moins 6 caractères")]
+        [MinLength(8, ErrorMessage = "Le mot de passe doit contenir au moins 8 caractères")]
         public string Password { get; set; } = string.Empty;
 
     }
