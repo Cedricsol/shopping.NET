1f588bcc2a8477c873302072de6d8066e80f8f79
Cédric <cedric.gautier@consultants-solutec.fr>
2026-05-06 17:22:35 +0200

    add setup for testing authorization on backend

---
+++ Shopping.NET.Tests/CustomWebApplicationFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Shopping.NET.Models;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Microsoft.AspNetCore.Hosting;
+
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            var descriptor = services.SingleOrDefault(
+                d => d.ServiceType == typeof(DbContextOptions<AppDbContext>));
+
+            if (descriptor != null)
+            {
+                services.Remove(descriptor);
+            }
+
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseInMemoryDatabase("TestDb");
+            });
+        });
+    }
+}
---
+++ Shopping.NET.Tests/JwtTestHelper.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Shopping.NET.Tests
+{
+    public static class JwtTestHelper
+    {
+        public static string GenerateToken(string role)
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name, "test"),
+                new Claim(ClaimTypes.Email, "test@test.com"),
+                new Claim(ClaimTypes.Role, role)
+            };
+
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes("super_secret_key_for_testing_123456789"));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: "test",
+                audience: "test",
+                claims: claims,
+                expires: DateTime.UtcNow.AddHours(1),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
